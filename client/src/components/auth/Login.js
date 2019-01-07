02ff2bb7770a1b57d8f74b7a3e8875dbd6579285
Kyle Booth <kbooth1000@gmail.com>
2019-01-06 22:02:53 -0500

    Start React login/register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,38 @@
 import React, {Component} from 'react';
 
 class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.onSubmitHandler=this.onSubmitHandler.bind(this);
+        this.onChangeHandler=this.onChangeHandler.bind(this);
+    }
+
+    onChangeHandler(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+    onSubmitHandler(e){
+        e.preventDefault();
+        const userLogin = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(userLogin);
+    }
     render(){
         return (
             <div className="Login">
                 <h1>Login</h1>
+                <form onSubmit={this.onSubmitHandler}>
+                    <input value={this.state.email} type="text" onChange={this.onChangeHandler} name="email" placeholder="Email" />
+                    <br/><input value={this.state.password} type="text" onChange={this.onChangeHandler} name="password" placeholder="Password" />
+                    <br/><input type="submit" />
+                </form>
             </div>
         )
     }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,43 @@
 import React, {Component} from 'react';
 
 class Register extends Component {
+    constructor(){
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {}
+        }
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    }
+    onChangeHandler(e){
+        this.setState({ [e.target.name] : e.target.value});
+    }
+    onSubmitHandler(e){
+        e.preventDefault();
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        }
+        console.log(newUser);
+    }
     render(){
         return (
             <div className="Register">
                 <h1>Register</h1>
+
+                <form onSubmit={this.onSubmitHandler}>
+                    <input type="text" value={this.state.name} placeholder="Name" name="name" onChange={this.onChangeHandler} className="form-name" /><br/>
+                    <input type="text" value={this.state.email} placeholder="Email" name="email" onChange={this.onChangeHandler} className="form-email" /><br/>
+                    <input type="text" value={this.state.password} placeholder="Password" name="password" onChange={this.onChangeHandler} className="form-password" /><br/>
+                    <input type="text" value={this.state.password2} placeholder="Confirm Password" name="password2" onChange={this.onChangeHandler} className="form-password2" />
+                    <br/><input type="submit" />
+                </form>
             </div>
         )
     }
