4522eef1dace5a899e04363bdca36b1c9d4a68d1
Kyle Booth <kbooth1000@gmail.com>
2019-01-13 14:01:57 -0500

    add thunk middleware

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk;'
+import thunk from 'redux-thunk';
 
 import Main from './components/Main';
 
